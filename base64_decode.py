6ea35e741de8cf98a2963f7fd0c1211ddb53ed7a
Johannes Schröter <info@devwerks.net>
2016-12-30 18:13:53 +0100

    added support for file input

--- base64_decode.py
+++ base64_decode.py
@@ -7,7 +7,7 @@ def decode():
     outFile = 0
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:s:", ["help", "output=", "string="])
+        opts, args = getopt.getopt(sys.argv[1:], "ho:i:s:", ["help", "output=", "input=", "string="])
 
     except getopt.GetoptError:
         help()
@@ -22,31 +22,39 @@ def decode():
         elif opt in ("-o", "--output"):
             outFile = open(arg, 'w')
             writeFile = True
+			
+        elif opt in ("-i", "--input"):
+            inFile = open(arg, "r")
+            inString = inFile.read()
+            output = base64.b64decode(inString)
 
+            if writeFile == True:
+                outFile.write(str(output, "utf-8"))
+
+            elif writeFile == False:
+                sys.stdout.write("\n%s\n" % output)
+			
         elif opt in ("-s", "--string"):
             output = base64.b64decode(arg)
 
             if writeFile == True:
-                outFile.write(output)
+                outFile.write(str(output, "utf-8"))
 
             elif writeFile == False:
                 sys.stdout.write("\n%s\n" % output)
 
-
 def version():
-    sys.stdout.write("\nBASE64 decode Script 0.1\n")
+    sys.stdout.write("\nBASE64 decode Script 0.2\n")
     sys.stdout.write("decode BASE64 string or file\n")
     sys.stdout.write("Author: Johannes Schroeter - www.devwerks.net\n\n")
 
-
 def help():
-    sys.stdout.write("base64_decode.py -o/--output FILEPATH -s/--string BASE64STRING\n")
+    sys.stdout.write("base64_decode.py -o/--output FILEPATH -i/--input INPUTFILE -s/--string BASE64STRING\n")
     sys.stdout.write("Example: base64_decode.py -s dGVzdA==\n\n")
 
 def main():
     decode()
     sys.exit()
-
 
 if __name__ == "__main__":
     main()
